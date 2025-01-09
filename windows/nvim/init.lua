136640058f1397aa5da166318f5a3704d889de2f
Tagamydev <samusanc@student.42madrid.com>
2025-01-09 22:23:01 +0100

    uploading the config

--- windows/nvim/init.lua
+++ windows/nvim/init.lua
@@ -1,5 +1,17 @@
-
+print("tagamy neovim config...")
 vim.opt.number = true;
 vim.opt.relativenumber = true
-vim.cmd.colorscheme('catppuccin_frappe');
+
+local call = vim.call
+local cmd = vim.cmd
+local Plug = vim.fn['plug#']
+local PATH = "~/.config/nvim/Plugged/"
+
+call('plug#begin', PATH)
+	Plug 'folke/tokyonight.nvim'
+call'plug#end'
+
+--vim.cmd.colorscheme('catppuccin_frappe');
+vim.cmd.colorscheme('tokyonight-storm');
 vim.cmd [[hi normal guibg=NONE ctermbg=NONE]];
+
