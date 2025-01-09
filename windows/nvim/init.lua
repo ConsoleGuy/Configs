5786fbacc387776977f63389d62592732cc8a50d
Tagamydev <samusanc@student.42madrid.com>
2025-01-09 22:24:52 +0100

    uploading git config

--- windows/nvim/init.lua
+++ windows/nvim/init.lua
@@ -5,7 +5,7 @@ vim.opt.relativenumber = true
 local call = vim.call
 local cmd = vim.cmd
 local Plug = vim.fn['plug#']
-local PATH = "~/.config/nvim/Plugged/"
+local PATH = "~/AppData/Local/nvim/Plugged"
 
 call('plug#begin', PATH)
 	Plug 'folke/tokyonight.nvim'
